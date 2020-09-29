f551fa1d1272cec3f2291c3c4eb732dfcc14dd77
shhu21 <hu.sara21@gmail.com>
2020-09-29 14:12:27 -0700

    adds tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,16 +22,14 @@ function Nav() {
     return (
         <header>
             <h2>
-                <a href="/">
-                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                <a data-testid="link" href="/">
+                    <span role="img" aria-label="camera"> 📸</span>Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                 <li className="mx-2">
-                    <a href="#about">
-                    About me
-                    </a>
+                    <a data-testid="about" href="#about">About me</a>
                 </li>
                 <li>
                     <span>Contact</span>
